2e24f2f4bae8badc068e8bbb9b3b258a5a46a551
masayuki-abe <cafenorthfox@gmail.com>
2022-09-01 22:55:52 +0900

    グロナビ用コンポネント作成

---
+++ components/organisms/TheGlobalNavi.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="l-globalNavi">
+    <li>
+      <a href="/">home</a>
+    </li>
+    <li>
+      <a href="#">page</a>
+    </li>
+    <li>
+      <a href="#">page</a>
+    </li>
+    <li>
+      <a href="#">page</a>
+    </li>
+    <li>
+      <a href="#">page</a>
+    </li>
+  </ul>
+</template>
--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <header class="l-header">
     <h1>Header</h1>
+    <OrganismsTheGlobalNavi />
   </header>
 </template>
